b3521c82d0db69f3e35b033d943080ef7890efc9
abbasnaumani <abbasnaumani@gmail.com>
2021-01-05 18:09:55 +0500

    Theme configurations

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,60 +1,59 @@
 <x-backend-layout>
     <!-- Hero -->
-        <div class="bg-body-light">
-            <div class="content content-full">
-                <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
-                    <h1 class="flex-sm-fill h3 my-2">Dashboard</h1>
-                    <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
-                        <ol class="breadcrumb breadcrumb-alt">
-                            <li class="breadcrumb-item">App</li>
-                            <li class="breadcrumb-item" aria-current="page">
-                                <a class="link-fx" href="">Dashboard</a>
-                            </li>
-                        </ol>
-                    </nav>
-                </div>
+    <div class="bg-body-light">
+        <div class="content content-full">
+            <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
+                <h1 class="flex-sm-fill h3 my-2">Dashboard</h1>
+                <nav class="flex-sm-00-auto ml-sm-3" aria-label="breadcrumb">
+                    <ol class="breadcrumb breadcrumb-alt">
+                        <li class="breadcrumb-item">App</li>
+                        <li class="breadcrumb-item" aria-current="page">
+                            <a class="link-fx" href="">Dashboard</a>
+                        </li>
+                    </ol>
+                </nav>
             </div>
         </div>
-        <!-- END Hero -->
-
-<<<<<<< HEAD
-        <!-- Page Content -->
-        <div class="content">
-            <div class="row justify-content-center">
-                <div class="col-md-6 col-xl-5">
-                    <div class="block">
-                        <div class="block-header">
-                            <h3 class="block-title">Welcome to your app</h3>
-                        </div>
-                        <div class="block-content">
-                            <p class="font-size-sm text-muted">
-                                We’ve put everything together, so you can start working on your Laravel project as soon
-                                as possible! OneUI assets are integrated and work seamlessly with Laravel Mix, so you
-                                can use the npm scripts as you would in any other Laravel project.
-                            </p>
-                            <p class="font-size-sm text-muted">
-                                Feel free to use any examples you like from the full versions to build your own pages.
-                                <strong>Wish you all the best and happy coding!</strong>
-                            </p>
-                        </div>
+    </div>
+    <!-- END Hero -->
+    <!-- Page Content -->
+    <div class="content">
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-xl-5">
+                <div class="block">
+                    <div class="block-header">
+                        <h3 class="block-title">Welcome to your app</h3>
+                    </div>
+                    <div class="block-content">
+                        <p class="font-size-sm text-muted">
+                            We’ve put everything together, so you can start working on your Laravel project as soon
+                            as possible! OneUI assets are integrated and work seamlessly with Laravel Mix, so you
+                            can use the npm scripts as you would in any other Laravel project.
+                        </p>
+                        <p class="font-size-sm text-muted">
+                            Feel free to use any examples you like from the full versions to build your own pages.
+                            <strong>Wish you all the best and happy coding!</strong>
+                        </p>
                     </div>
                 </div>
-=======
-    </div>
-    <div class="modal fade" id="MyPopup" role="dialog" data-backdrop="static" data-keyboard="false">
-        <div class="modal-dialog">
-        
-          <!-- Modal content-->
-          <div class="modal-content">
-            <div class="modal-header">
-          
-        </div>
-        <div class="modal-body"></div>
-            <div class="modal-footer" style="text-align: center;">
-              <button type="button" class="yes-btn btn btn-default" >Yes</button>
-              <button type="button" class="no-btn btn btn-default" >No</button>
->>>>>>> 27e5d67356107204b015dc4cb8cbb075cd2dd7b3
             </div>
         </div>
-        <!-- END Page Content -->
+        <div class="modal fade" id="MyPopup" role="dialog" data-backdrop="static" data-keyboard="false">
+            <div class="modal-dialog">
+
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+
+                    </div>
+                    <div class="modal-body"></div>
+                    <div class="modal-footer" style="text-align: center;">
+                        <button type="button" class="yes-btn btn btn-default">Yes</button>
+                        <button type="button" class="no-btn btn btn-default">No</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- END Page Content -->
 </x-backend-layout>
