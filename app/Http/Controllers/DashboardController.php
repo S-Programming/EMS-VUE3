f76b3f975d6497defcbf6dc95dc354d639e2274d
abbasnaumani <abbasnaumani@gmail.com>
2021-01-16 19:12:51 +0500

    dashbaord data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -38,36 +38,7 @@ class DashboardController extends Controller
      */
     public function index(Request $request)
     {
-        // Current Month Checkins
-        $userId = $this->getAuthUserId();
-        $monthlyCheckins = CheckinHistory::where('checkin', '>=', Carbon::now()->startOfMonth()->toDateTimeString())
-            ->where('user_id', $userId)
-            ->get()->count();
-        $previousMonthCheckins = CheckinHistory::whereMonth(
-            'checkin',
-            '=',
-            Carbon::now()->subMonth()->month
-        )->get()->count();
-        $NowDate = Carbon::now()->format('Y-m-d');
-        $currentStartWeekDate = Carbon::now()->subDays(Carbon::now()->dayOfWeek - 1); // gives 2016-01-3
-        $currentWeekCheckins = CheckinHistory::whereBetween('checkin', array($currentStartWeekDate, $NowDate))
-            ->where('user_id', $userId)
-            ->get()->count();
-        $previousWeekStartDate = Carbon::now()->subDays(Carbon::now()->dayOfWeek - 1)->subWeek()->format('Y-m-d'); // gives 2016-01-31
-        $previousWeekEndDate = Carbon::now()->subDays(Carbon::now()->dayOfWeek)->format('Y-m-d');
-        $pastWeekCheckins = CheckinHistory::whereBetween('checkin', array($previousWeekStartDate, $previousWeekEndDate))
-            ->where('user_id', $userId)
-            ->get()->count();
-        // Total Users
-        $count = DB::table('users')->count();
-        $user = $this->getAuthUser();
-        $responseData = ['is_checkin' => $this->isUserCheckin(),'user' => $user,'count' => $count, 'monthlyCheckins' => $monthlyCheckins, 'previousMonthCheckins' => $previousMonthCheckins, 'currentWeekCheckins' => $currentWeekCheckins, 'pastWeekCheckins' => $pastWeekCheckins];
-        $responseData['checkin_history'] = $user ? $user->checkinHistory : null;
-        $responseData['is_checkin'] = $this->isUserCheckin();
-        if ($responseData['is_checkin']) {
-            $responseData['user_last_checkin_time'] = $this->userLastCheckinTime();
-        }
-        return view('pages.user.dashboard', $responseData);
+        return $this->dashboardService->index($request);
     }
 
     /**
@@ -78,6 +49,9 @@ class DashboardController extends Controller
     public function dashboard()
     {
         return $this->dashboardService->getDashboard();
+
+
+
     }
 
 
