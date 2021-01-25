5692472746d95e10aea8a7767251461ea675ec26
devhassam <developer.hassam@gmail.com>
2021-01-25 19:42:40 +0500

    UI Fixed by Hassam

---
+++ public/js/oneui-helpers.js
@@ -0,0 +1,5 @@
+/* Page JS Helpers (jQuery Plugins) */
+jQuery(function () {
+    One.helpers(['flatpickr', 'datepicker', 'colorpicker', 'maxlength', 'select2', 'masked-inputs', 'rangeslider', 'sparkline', ]);
+});
+
