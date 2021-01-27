284cbd29ce8462fbef36efd480c74dc241d5c42a
S-Programming <test>
2021-01-27 13:38:59 +0500

    Holidays and Leaves

--- routes/Http/Holiday.php
+++ routes/Http/Holiday.php
@@ -2,7 +2,7 @@
 
 namespace Route\Http;
 
-use App\Http\Controllers\PublicHolidayController;
+use App\Http\Controllers\HolidayController;
 use \Illuminate\Support\Facades\Route;
 
 class Holiday
@@ -10,17 +10,17 @@ class Holiday
     static function register()
     {
         Route::group(['middleware' => ['auth:sanctum']], function () {
-            Route::get('/holiday', [PublicHolidayController::class, 'index'])->name('holiday.list');
-            Route::post('/add_holiday_modal', [PublicHolidayController::class, 'holidayModal'])->name('holiday.add.modal');
+            Route::get('/holiday', [HolidayController::class, 'index'])->name('holiday.list');
+            Route::post('/add_holiday_modal', [HolidayController::class, 'holidayModal'])->name('holiday.add.modal');
 
-            Route::post('/confirm_add_holiday', [PublicHolidayController::class, 'confirmAddHoliday'])->name('holiday.confirm.add');
+            Route::post('/confirm_add_holiday', [HolidayController::class, 'confirmAddHoliday'])->name('holiday.confirm.add');
 
-            Route::post('/edit_holiday_modal', [PublicHolidayController::class, 'holidayEditModal'])->name('holiday.edit.modal');
-            Route::post('/update_holiday_modal', [PublicHolidayController::class, 'holidayUpdate'])->name('holiday.update');
+            Route::post('/edit_holiday_modal', [HolidayController::class, 'holidayEditModal'])->name('holiday.edit.modal');
+            Route::post('/update_holiday_modal', [HolidayController::class, 'holidayUpdate'])->name('holiday.update');
 
-            Route::post('/delete_holiday_modal', [PublicHolidayController::class, 'holidayDeleteModal'])->name('holiday.delete.modal');
+            Route::post('/delete_holiday_modal', [HolidayController::class, 'holidayDeleteModal'])->name('holiday.delete.modal');
 
-            Route::post('/confirm_delete_holiday', [PublicHolidayController::class, 'confirmDeleteHoliday'])->name('holiday.confirm.delete');
+            Route::post('/confirm_delete_holiday', [HolidayController::class, 'confirmDeleteHoliday'])->name('holiday.confirm.delete');
           
            
         });
