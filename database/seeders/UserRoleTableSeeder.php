ffe2472d3e262b05e4a9f16b20b800c9c0675360
S-Programming <test>
2021-01-29 20:28:45 +0500

    Expense and FOrgot Password and Logout

--- database/seeders/UserRoleTableSeeder.php
+++ database/seeders/UserRoleTableSeeder.php
@@ -16,6 +16,7 @@ class UserRoleTableSeeder extends Seeder
             ['user_id' => 1, 'role_id' => 1],
             ['user_id' => 2, 'role_id' => 2],
             ['user_id' => 3, 'role_id' => 3],
+            ['user_id' => 4, 'role_id' => 3],
         ];
         DB::table('role_user')->insert($userRoles);
     }
--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -17,6 +17,7 @@ class UsersTableSeeder extends Seeder
             ['id' => 1, 'first_name' => 'Super', 'last_name' => 'Admin', 'email' => 'abbasnaumani+1@gmail.com', 'phone_number' => '4545454545', 'password' => bcrypt('12345678'), 'is_active' => 1, 'is_approved' => 1, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
             ['id' => 2, 'first_name' => 'Admin', 'last_name' => 'User', 'email' => 'abbasnaumani+2@gmail.com', 'phone_number' => '4545454545', 'password' => bcrypt('12345678'), 'is_active' => 1, 'is_approved' => 1, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
             ['id' => 3, 'first_name' => 'Simple', 'last_name' => 'User', 'email' => 'abbasnaumani+3@gmail.com', 'phone_number' => '4545454545', 'password' => bcrypt('12345678'), 'is_active' => 1, 'is_approved' => 1, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
+            ['id' => 4, 'first_name' => 'Muhammad', 'last_name' => 'Saddique', 'email' => 'saddiquearain99@gmail.com', 'phone_number' => '+923161880989', 'password' => bcrypt('12345678'), 'is_active' => 1, 'is_approved' => 1, 'created_at' => Carbon::now()->format('Y-m-d H:i:s')],
 
         ];
         DB::table('users')->insert($users);
