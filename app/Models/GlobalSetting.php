7f812ea72f5592115ed671daac9045bd377d2c35
abbasnaumani <abbasnaumani@gmail.com>
2021-04-30 16:14:35 +0500

    Updated data

--- app/Models/GlobalSetting.php
+++ app/Models/GlobalSetting.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class GlobalSetting extends Model
 {
     use HasFactory;
-    public $fillable = ['display_name','field_name','checkin_time','checkin_margin','working_hours','working_hour_margin','value','comments','value_type','created_at','updated_at'];
+    public $fillable = ['display_name','field_name','value','comments','value_type','created_at','updated_at'];
     protected $table = 'global_settings';
 }
